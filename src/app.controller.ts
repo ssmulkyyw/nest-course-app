6b0c816af3378638d314704560814deacd898c41
Irina <gerteyirina123@gmail.com>
2025-02-14 10:32:08 +0700

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get() // localhost:3000
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get('/showjson') //localhost:3000/showjson
+  getJson() {
+    return this.appService.getJson();
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'We love NestJS';
+  }
+  getJson() {
+    return {
+      name: 'Irina',
+      lastname: 'Gertye',
+      age: 99,
+    };
+}
+}
